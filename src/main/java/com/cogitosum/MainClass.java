7236443164d164f8f225c65b3df0bea2cbd70b18
JANS Daniel <djans@cogitosum.com>
2024-01-03 18:37:49 -0500

    Commit 2

--- src/main/java/com/cogitosum/MainClass.java
+++ src/main/java/com/cogitosum/MainClass.java
@@ -17,7 +17,7 @@ public class MainClass {
    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
       return args -> {
 
-         System.out.println("Let's inspect the beans provided by Spring Boot 2:");
+         System.out.println("Let's inspect the beans provided by Spring Boot 23432433:");
 
          String[] beanNames = ctx.getBeanDefinitionNames();
          Arrays.sort(beanNames);
